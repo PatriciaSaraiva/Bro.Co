16a530a173bf96f7acd330830185b63c0ca33aae
patriciasaraiva <patricia-saraiva_student2022@wilder.school>
2022-03-18 09:45:57 +0000

    add css code & burgerMenu.js file

--- components/burger_menu.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector(".burger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    burger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
