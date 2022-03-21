c89cc84bbbbe58c9a5276734463e2bc4afdfc6ab
patriciasaraiva <patricia-saraiva_student2022@wilder.school>
2022-03-21 09:36:36 +0000

    update newsletter file

--- components/burgerMenu.js
+++ components/burgerMenu.js
@@ -1,9 +1,11 @@
 const burger = document.querySelector(".burger");
 const navMenu = document.querySelector(".nav-menu");
 
+
 function mobileMenu() {
     burger.classList.toggle("active");
     navMenu.classList.toggle("active");
+   
 }
 
 burger.addEventListener("click", mobileMenu);
@@ -15,4 +17,6 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 function closeMenu() {
     burger.classList.remove("active");
     navMenu.classList.remove("active");
+   
+   
 }
